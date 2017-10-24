847f12c1435e10cc940fda363b62a7b049d07790
Felipe Zapata <tifonzafel@gmail.com>
2017-10-24 18:31:07 +0200

    modyfing the examples

--- example_2.py
+++ example_2.py
@@ -3,41 +3,42 @@ from qmflows import (Settings, adf, dftb, run, templates)
 from qmflows.molkit import from_smiles
 
 # List of Molecules to simulate
-smiles = [
-        'C1=CC2=C(C=C1)C=CC=C2',
-        'C1CC2(CCCCC2)C=C1',
-        'C1=CC2=CC=CC=CC2=C1',
-        'C1=CC=CC=C1',
-        'C1CCC=CC1',
-        'C1=CC=C(C=C1)OC1=CC=CC=C1',
-        'C1C=CC=CC=C1'
-]
+smiles = {'naphthalene': 'C1=CC2=C(C=C1)C=CC=C2',
+          'azulene': 'C1=CC2=CC=CC=CC2=C1',
+          'cycloheptatriene': 'C1=CC=C(C=C1)OC1=CC=CC=C1',
+          'furaldehyde': 'O=CC1=COC=C1',
+          'methylthiophene': 'CC1=C(C=CS1)C#C'}
+
 
 # transform the string into a format understandable by Qmflows
-molecules = map(from_smiles, smiles)
+molecules = {name: from_smiles(smile) for name, smile in smiles.items()}
 
 # Used DFTB to optimize the geometries
-dftb_jobs = [dftb(templates.geometry, mol) for mol in molecules]
-optimized_mols = [job.molecule for job in dftb_jobs]
+dftb_jobs = {name: dftb(templates.geometry, mol, name='dftb_{}'.format(name))
+             for name, mol in molecules.items()}
+optimized_mols = {name: job.molecule for name, job in dftb_jobs.items()}
 
 # Settings for ADF
 s = Settings()
 s.basis = 'DZP'
-s.functional = 'b3lyp'
+s.functional = 'pbe'
 s.specific.adf.scf.converge = 1e-6
 s.specific.adf.symmetry = 'nosym'
 
 # Compute the single point calculation
-singlepoints = [adf(s, mol) for mol in optimized_mols]
+singlepoints = {name: adf(s, mol, name='adf_{}'.format(name))
+                for name, mol in optimized_mols.items()}
 
 # Extract the HOMO-LUMO values
-homos_lumos = [gather(sp.homo, sp.lumo) for sp in singlepoints]
+homos_lumos = {name: gather(sp.homo, sp.lumo)
+               for name, sp in singlepoints.items()}
 
 # Run the computation
-results = run(gather(*homos_lumos))
+results = run(gather(**homos_lumos))
 
 # Compute the Gap
-gaps = [(lumo - homo) * 27.21 for homo, lumo in results]
+print(results)
+# gaps = [(lumo - homo) * 27.21 for homo, lumo in results]
 
 
-print("HOMO-LUMO gap (eV)", gaps)
+# print("HOMO-LUMO gap (eV)", gaps)
--- example_figure_2d.py
+++ example_figure_2d.py
@@ -1,14 +1,16 @@
 from scm.plams import Molecule
 from qmflows import (adf, run, templates)
 
-molecule = Molecule('uranyl.xyz')
+molecule = Molecule('files/uranyl.xyz')
 inp = templates.geometry
 
 # Generic Settings
-inp.basis = 'TZ2P'
+# inp.basis = 'TZ2P'
+inp.basis = 'DZP'
 inp.functional = 'bp86'
 
 # Specific
 inp.specific.adf.relativistic = "scalar Zora"
+inp.specific.adf.scf.iterations = 500
 
-result = run(inp, molecule)
+result = run(adf(inp, molecule))
