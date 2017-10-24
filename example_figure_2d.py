e3c8c58b7ab107ad336fbc0fc0a81f6dd9a4dd8b
Felipe Zapata <tifonzafel@gmail.com>
2017-10-24 23:30:17 +0200

    added charge to uranyl

--- example_figure_2d.py
+++ example_figure_2d.py
@@ -5,12 +5,12 @@ molecule = Molecule('files/uranyl.xyz')
 inp = templates.geometry
 
 # Generic Settings
-# inp.basis = 'TZ2P'
-inp.basis = 'DZP'
+inp.basis = 'TZ2P'
 inp.functional = 'bp86'
 
 # Specific
+inp.specific.adf.charge = "2  0"
 inp.specific.adf.relativistic = "scalar Zora"
-inp.specific.adf.scf.iterations = 500
+inp.specific.adf.geometry.convergence = 'grad=1e-4'
 
 result = run(adf(inp, molecule))
