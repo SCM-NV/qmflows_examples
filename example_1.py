cbff9c522d84a1ec0c320541774bf3e29fb9abc9
felipeZ <tifonzafel@gmail.com>
2017-10-24 11:20:36 +0200

    added cisd gamess to example 1

--- example_1.py
+++ example_1.py
@@ -1,7 +1,8 @@
 from scm.plams import Molecule
-from qmflows import (adf, dftb, run, templates)
+from qmflows import (
+    adf, dftb, gamess, run, templates, Settings)
 
-molecule = Molecule('acetonitrile.xyz')
+molecule = Molecule('files/acetonitrile.xyz')
 
 inp = templates.geometry
 
@@ -9,12 +10,22 @@ inp = templates.geometry
 dftb_opt = dftb(inp, molecule)
 
 # Geometry optimization with ADF
-inp.functional = "PBE"
+inp.functional = "pbe"
 inp.basis = "DZP"
-
 adf_opt = adf(inp, dftb_opt.molecule)
 
+# Single and double excitations CISD using Gamess
+cisd = Settings()
+cisd.specific.gamess.control.cityp = 'GUGA'
+cisd.specific.gamess.control.runtype = 'energy'
+cisd.specific.gamess.cidrt.group = 'c3v'
+cisd.specific.gamess.cidrt.iexcit = 2
+cisd.specific.gamess.basis.gbasis = 'sto'
+cisd.specific.gamess.basis.ngauss = 3
+
+gamess_cisd = gamess(cisd, adf_opt.molecule)
+
 # Running the results
-result = run(adf_opt.molecule)
+result = run(gamess_cisd.energy)
 
 print(result)
