f1148d49a9f25789dac60c610da49ccdae17d2f5
Felipe Zapata <tifonzafel@gmail.com>
2017-11-15 16:50:26 +0100

    updated example_1

--- example_1.py
+++ example_1.py
@@ -1,6 +1,6 @@
 from scm.plams import Molecule
 from qmflows import (
-    adf, dftb, gamess, run, templates, Settings)
+    adf, dftb, orca, run, templates, Settings)
 
 molecule = Molecule('files/acetonitrile.xyz')
 
@@ -14,18 +14,16 @@ inp.functional = "pbe"
 inp.basis = "DZP"
 adf_opt = adf(inp, dftb_opt.molecule)
 
-# Single and double excitations CISD using Gamess
-cisd = Settings()
-cisd.specific.gamess.control.cityp = 'GUGA'
-cisd.specific.gamess.control.runtype = 'energy'
-cisd.specific.gamess.cidrt.group = 'c3v'
-cisd.specific.gamess.cidrt.iexcit = 2
-cisd.specific.gamess.basis.gbasis = 'sto'
-cisd.specific.gamess.basis.ngauss = 3
+# Excited state geometry optimization
+cis = Settings()
+cis.basis = 'def2TZVP'
+cis.specific.orca.cis.Nroots = 1
+cis.specific.orca.cis.iroot = 1
+cis.specific.orca.main = 'RHF tightscf'
 
-gamess_cisd = gamess(cisd, adf_opt.molecule)
+orca_cis = orca(cis, adf_opt.molecule, job_name='cis')
 
 # Running the results
-result = run(gamess_cisd.energy)
+result = run(orca_cis.molecule, folder='cis')
 
 print(result)
