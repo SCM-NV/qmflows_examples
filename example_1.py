d9cef5147cb1e00aaa669b23c437abbb46a87ee3
Felipe Zapata <tifonzafel@gmail.com>
2018-01-15 15:48:03 +0100

    run EOM-CCSD single point after optimization in example 1

--- example_1.py
+++ example_1.py
@@ -2,7 +2,7 @@ from scm.plams import Molecule
 from qmflows import (
     adf, dftb, orca, run, templates, Settings)
 
-molecule = Molecule('files/acetonitrile.xyz')
+molecule = Molecule('files/chlorophyll_a.xyz')
 
 inp = templates.geometry
 
@@ -14,16 +14,13 @@ inp.functional = "pbe"
 inp.basis = "DZP"
 adf_opt = adf(inp, dftb_opt.molecule)
 
-# Excited state geometry optimization
-cis = Settings()
-cis.basis = 'def2TZVP'
-cis.specific.orca.cis.Nroots = 1
-cis.specific.orca.cis.iroot = 1
-cis.specific.orca.main = 'RHF tightscf'
+# single point calculation using EOMS-CCSD
+ccsd = Settings()
+ccsd.specific.orca.mdci.nroots = 10
+ccsd.specific.orca.main = 'RHF EOM-CCSD def2TZVP tightscf'
 
-orca_cis = orca(cis, adf_opt.molecule, job_name='cis')
+orca_ccsd = orca(ccsd, adf_opt.molecule, job_name='ccsd')
 
 # Running the results
-result = run(orca_cis.molecule, folder='cis')
+run(orca_ccsd, folder='ccsd')
 
-print(result)
